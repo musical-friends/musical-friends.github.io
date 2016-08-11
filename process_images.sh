b3eab85237483833a27eb81d73b41b1e8fc1e9ef
Reinhard Naegele <unguiculus@gmail.com>
2016-08-11 20:25:54 +0200

    Add raw images

--- process_images.sh
+++ process_images.sh
@@ -1,18 +1,18 @@
 #!/bin/bash -x
 
-rm -rf _assets/photos
-mkdir -p _assets/photos
+rm -rf _assets/images
+mkdir -p _assets/images
 
 pushd _raw
 
 for f in *.jpg; do
   filename="${f%%.*}"
-  convert -thumbnail 200x200 "$f" "../_assets/photos/$filename-thumb.png"
+  convert -thumbnail 200x200 "$f" "../_assets/images/$filename-thumb.png"
 done
 
 for f in *.jpg; do
   filename="${f%%.*}"
-  convert -resize 1024x1024 "$f" "../_assets/photos/$filename.jpg"
+  convert -resize 1024x1024 "$f" "../_assets/images/$filename.jpg"
 done
 
 popd

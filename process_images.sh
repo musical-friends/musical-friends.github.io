d1ef5494428e39d58853185765a40daf810ba3f3
Reinhard Nägele <unguiculus@gmail.com>
2017-01-17 21:21:54 +0100

    Update Jekyll

--- process_images.sh
+++ process_images.sh
@@ -5,11 +5,6 @@ pushd _raw
 for f in *.jpg; do
   filename="${f%%.*}"
   convert -thumbnail 280x280 "$f" "../_assets/images/$filename-thumb.png"
-  mv "$f" "processed/$f"
-done
-
-for f in *.jpg; do
-  filename="${f%%.*}"
   convert -resize 1024x1024 "$f" "../_assets/images/$filename.jpg"
   mv "$f" "processed/$f"
 done
